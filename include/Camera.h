bf8b6119051e36008a79f6663c3094a3dea5d659
bdwhst <60293727+bdwhst@users.noreply.github.com>
2020-12-16 23:57:41 +0800

    Update Camera.h

--- include/Camera.h
+++ include/Camera.h
@@ -1,11 +1,14 @@
 #ifndef CAMERA_H
 #define CAMERA_H
 
+#define PI 3.14159265
+
 #include <glad/glad.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
 #include <vector>
+#include <math.h>
 
 // Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
 enum Camera_Movement {
